bead6619258aa6c41812d0fcc458db27b996d2c0
irvan syah <irvansyah126@gmail.com>
2026-06-17 14:29:30 +0700

    ada cors

--- taskmanager/src/main/java/com/enterprise/taskmanager/controller/TaskController.java
+++ taskmanager/src/main/java/com/enterprise/taskmanager/controller/TaskController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/tasks")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5175")
 public class TaskController {
 
     private final TaskService taskService;
